7d62bf3fab6dbe97a3b02b8bc35c54dc041fbca1
imamah <54374040+ImamahZafar@users.noreply.github.com>
2019-10-16 12:09:36 +0500

    first

---
+++ base/src/main/java/com/ahmad/cv/fragments/Achievement.java
@@ -0,0 +1,65 @@
+package com.ahmad.cv.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ahmad.cv.adapters.AchievementAdapter;
+import com.ahmad.cv.adapters.EducationAdapter;
+import com.ahmad.cv.base.R;
+import com.ahmad.cv.model.DemoItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Achievement extends Fragment {
+
+    public Achievement() {
+        // Required empty public constructor
+    }
+
+
+    public static Achievement newInstance() {
+        Achievement fragment = new Achievement();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_achievement, container, false);
+        RecyclerView recyclerViewDemo = view.findViewById(R.id.recyclerViewDemo4);
+        recyclerViewDemo.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewDemo.setAdapter(new AchievementAdapter(feedItems(),getContext()));
+        return view;
+    }
+
+
+
+    private List<DemoItem> feedItems() {
+
+        String Titles = "Achievement Section" ;
+        List<DemoItem> demoItems = new ArrayList<>();
+        DemoItem demoItem = new DemoItem(Titles);
+        demoItems.add(demoItem);
+        return demoItems;
+    }
+}
